74e77e0a2d2629a09e8d9d8936484eb06fc8900a
Tuomas Rautiola <tuomas.rautiola@gmail.com>
2024-09-25 18:53:08 +0300

    getting build and run docker script from master (#2)
    * removed the file that is in .gitignore
    
    * Feature/add docker build and run script (#1)
    
    * added bash script to build and run the docker
    
    * modified the stopping if
    
    * another small mod to the build script
    
    * another mod
    
    * moving getting image and container names to .env file

---
+++ buildAndRunDocker.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Define variables
+#IMAGE_NAME="youtubebot"
+#CONTAINER_NAME="youtubebot-container"
+# Load variables from .env file
+if [ -f .env ]; then
+    export $(cat .env | grep -v '^#' | xargs)
+else
+    echo ".env file not found!"
+    exit 1
+fi
+
+
+# Step 1: Build the Docker image
+echo "Building Docker image..."
+docker build -t $IMAGE_NAME .
+
+# Step 2: Stop and remove existing container (if any)
+# Check if the container is running
+if [ -n "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
+    echo "Stopping running container..."
+    docker stop $CONTAINER_NAME
+fi
+
+# Remove the container (whether stopped or exited)
+if [ -n "$(docker ps -a -q -f name=$CONTAINER_NAME)" ]; then
+    echo "Removing existing container(s)..."
+    docker rm $CONTAINER_NAME
+fi
+
+# Step 3: Run the new container
+echo "Running the new container..."
+docker run -d --name $CONTAINER_NAME $IMAGE_NAME
+
+echo "Container is running!"
