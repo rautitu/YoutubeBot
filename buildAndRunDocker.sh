cc4555ed0936fa905f6ce57365b006e9b5f995b6
Tuomas Rautiola <tuomas.rautiola@gmail.com>
2024-09-27 15:26:42 +0300

    Bugfix/fixing logging loop with restart (#9)
    * trying to fix log_container to allow exiting with code 0
    
    * another attempt
    
    * another attempt
    
    * switching to not having logs extracted after a restart
    
    * removed one buggy row
    
    * was missing & to force background execution
    
    * added -t option to docker logs
    
    * added some comment stating that logs wont be stored if a restart occurs with "restart" command or after restarting after hitting an error

--- buildAndRunDocker.sh
+++ buildAndRunDocker.sh
@@ -48,26 +48,14 @@ docker run \
     $IMAGE_NAME
 
 # Step 4: Attaching a logging mechanism and a cleanup mechanism for old logs
-# Function to attach logs and keep logging
-log_container() {
-  while true; do
-    if docker ps --filter "name=$CONTAINER_NAME" --filter "status=running" | grep -q "$CONTAINER_NAME"; then
-      # Attach logs if the container is running
-      docker logs -f "$CONTAINER_NAME" >> "${LOG_DIR}/${CONTAINER_NAME}/container_${LOG_TIMESTAMP}.log" 2>&1
-    else
-      echo "Waiting for container to restart..."
-      sleep 5  # Wait and retry if the container is not running yet
-    fi
-  done
-}
+#NOTE: If docker restarts, new logs will not be stored for that session. Reason is that this command does not run during restart
+#and I found no way to run this during restart. Probably some heartneat approach would be the way.
+docker logs -t -f "$CONTAINER_NAME" >> "${LOG_DIR}/${CONTAINER_NAME}/container_${LOG_TIMESTAMP}.log" 2>&1 &
 
 # Function to cleanup log files that are older than 60 days
 cleanup_logs() {
     find "${LOG_DIR}/${CONTAINER_NAME}" -type f -name "container_*.log" -mtime +60 -exec rm {} \;
 }
-
-# Start logging
-log_container &
 
 # Call the cleanup function after starting logging
 cleanup_logs
