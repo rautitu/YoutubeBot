80a6a8c13c2dd853580f9e59bce45b8cf23e1bca
Tuomas Rautiola <ext-tuomas.rautiola@insta.fi>
2025-12-02 14:38:29 +0200

    removed showing of live logs

--- buildAndRunDocker.sh
+++ buildAndRunDocker.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Load .env file
 if [ -f .env ]; then
     export $(cat .env | grep -v '^#' | xargs)
 else
@@ -8,10 +7,10 @@ else
     exit 1
 fi
 
-# Create the log directory on the host
+#creating log directory on the host if it doesnt exist
 mkdir -p "${LOG_DIR}/${CONTAINER_NAME}"
 
-# Build + recreate container (unless argument is "no-build")
+#build + recreate container (unless argument is "no-build")
 if [ "$1" != "no-build" ]; then
     docker compose build
 fi
@@ -20,7 +19,7 @@ docker compose down          # Stop & remove container cleanly
 docker compose up -d         # Start detached
 
 # Optional: show logs live (but compose already timestamps them)
-docker compose logs -f youtubebot &
+#docker compose logs -f youtubebot &
 
 # Cleanup logs older than 60 days:
 find "${LOG_DIR}/${CONTAINER_NAME}" \
